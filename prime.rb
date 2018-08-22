750b88ac283f02774ee27d650f450be1b96862cc
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 20:34:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 # Add  code here!
 def prime?(n)
+  puts n 
   return true if n == 2
   for i in 2..n do 
+    puts i 
     if n % i == 0 
       return true 
     end 
   end 
+  puts ''
   false 
 end 
